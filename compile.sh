a39f1de8f3070d5e61428027edca2e0bd3903bf2
nelsasser <elsanic96@gmail.com>
2023-03-08 13:39:23 -0700

    setup workflow for sharing

---
+++ compile.sh
@@ -0,0 +1,8 @@
+export BIBINPUTS=/Users/nelsasser/Documents/thesis-manuscript/src
+export BSTINPUTS=/Users/nelsasser/Documents/thesis-manuscript/src:
+cd ./src && pdflatex -file-line-error -interaction=nonstopmode -synctex=1 -output-directory=../out thesis.tex && cd ..
+cd ./out/ && bibtext thesis && cd ..
+cd ./src
+pdflatex -file-line-error -interaction=nonstopmode -synctex=1 -output-directory=../out thesis.tex
+pdflatex -file-line-error -interaction=nonstopmode -synctex=1 -output-format=pdf -output-directory=../out thesis.tex
+cp ../out/thesis.pdf ../thesis.pdf
