3d75e9a7c2f7af27cac251a83a5d925aff905b7a
Nick Elsasser <elsanic96@gmail.com>
2023-04-20 16:50:26 -0600

    more cleanup

--- compile.sh
+++ compile.sh
@@ -1,5 +1,7 @@
-export BIBINPUTS=/Users/nelsasser/Documents/thesis-manuscript/src
-export BSTINPUTS=/Users/nelsasser/Documents/thesis-manuscript/src:
+#export BIBINPUTS=/Users/nelsasser/Documents/thesis-manuscript/src
+#export BSTINPUTS=/Users/nelsasser/Documents/thesis-manuscript/src:
+export BIBINPUTS=~/IdeaProjects/ads/thesis-manuscript/src
+export BSTINPUTS=~/IdeaProjects/ads/thesis-manuscript/src
 cd ./src && pdflatex -file-line-error -interaction=nonstopmode -synctex=1 -output-directory=../out thesis.tex && cd ..
 cd ./out/ && bibtext thesis && cd ..
 cd ./src
